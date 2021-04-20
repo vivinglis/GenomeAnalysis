4aac69cd37e1fd1589721778e83b02c8115aa91e
Vivianne Inglis <vivianne.inglis@gmail.com>
2021-04-20 23:31:21 +0200

    Prepped scripts to run tomorrow - trimming of RNA and fastqc of trimmed RNA reads

---
+++ code/fastqcRNAtrim.sh
@@ -0,0 +1,6 @@
+# Script to run fastQC on trimmed RNA reads
+
+for files in ~/genome_analysis/GenomeAnalysis/data/trimmed_data/RNA_trimmed/*
+do
+fastqc $files -o ~/genome_analysis/GenomeAnalysis/analysis/01_preprocessing/fastqc_trim/RNA_trim
+done
--- code/trim_RNA.sh
+++ code/trim_RNA.sh
@@ -1,3 +1,19 @@
 # trim RNA reads using trimmomatic
+module load bioinfo-tools
+module load trimmomatic
 
-java -jar trimmomatic-0.39.jar PE input_forward.fq.gz input_reverse.fq.gz output_forward_paired.fq.gz output_forward_unpaired.fq.gz output_reverse_paired.fq.gz output_reverse_unpaired.fq.gz ILLUMINACLIP:TruSeq3-PE.fa:2:30:10:2:keepBothReads LEADING:3 TRAILING:3 MINLEN:36
+cd ~/genome_analysis/GenomeAnalysis/data/trimmed_data/RNA_trimmed/
+
+java -jar trimmomatic-0.39.jar PE \
+~/genome_analysis/GenomeAnalysis/data/raw_data/RNA_raw/SRR4342137.1.fastq.gz \
+~/genome_analysis/GenomeAnalysis/data/raw_data/RNA_raw/SRR4342137.2.fastq.gz \
+RNA_trim_D1_1.paired.trimmed.fastq.gz \
+RNA_trim_D1_2.paired.trimmed.fastq.gz \
+ILLUMINACLIP:TruSeq3-PE.fa:2:30:10:2:keepBothReads LEADING:3 TRAILING:3 MINLEN:36
+
+java -jar trimmomatic-0.39.jar PE \
+~/genome_analysis/GenomeAnalysis/data/raw_data/RNA_raw/SRR4342139.1.fastq.gz \
+~/genome_analysis/GenomeAnalysis/data/raw_data/RNA_raw/SRR4342139.2.fastq.gz \
+RNA_trim_D3_1.paired.trimmed.fastq.gz \
+RNA_trim_D3_2.paired.trimmed.fastq.gz \
+ILLUMINACLIP:TruSeq3-PE.fa:2:30:10:2:keepBothReads LEADING:3 TRAILING:3 MINLEN:36
