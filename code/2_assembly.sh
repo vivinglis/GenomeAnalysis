9a3f9955c652c6f2029a564cfe7550df7620b056
Vivianne Inglis <vinglis@rackham3.uppmax.uu.se>
2021-04-21 16:14:48 +0200

    Tried to run megahit, but it all went tits up. Going to try again.

--- code/2_assembly.sh
+++ code/2_assembly.sh
@@ -3,8 +3,8 @@
 #SBATCH -M snowy
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 00:20:00
-#SBATCH -J megahit_assembly_D1
+#SBATCH -t 20:00:00
+#SBATCH -J megahit_assembly
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user vivianne.inglis.0395@student.uu.se
 
@@ -15,15 +15,18 @@ module load megahit
 # Paper's parameters (IBDA-UB) -mink 65 -maxk 105 -step 10.
 # TAs recommended --kmin-1pass
 
+cd ~/genome_analysis/GenomeAnalysis/analysis/02_assembly
+
+
 # Assembly for DNA from site D1
-megahit --k-min 65 --k-max 105 --k-step 10 --kmin-1pass \ 
+megahit --k-min 65 --k-max 105 --k-step 10 --kmin-1pass \
 -1 ~/genome_analysis/GenomeAnalysis/data/trimmed_data/DNA_trimmed/SRR4342129_1.paired.trimmed.fastq.gz \
 -2 ~/genome_analysis/GenomeAnalysis/data/trimmed_data/DNA_trimmed/SRR4342129_2.paired.trimmed.fastq.gz \
--o ~/genome_analysis/GenomeAnalysis/analysis/02_assembly/megahit_output 2>D1_assembly_output
+-o D1_megahit_output 2>D1_assembly_output
 
 
 # Assembly for DNA from site D3
 megahit --k-min 65 --k-max 105 --k-step 10 --kmin-1pass \
 -1 ~/genome_analysis/GenomeAnalysis/data/trimmed_data/DNA_trimmed/SRR4342133_1.paired.trimmed.fastq.gz \
 -2 ~/genome_analysis/GenomeAnalysis/data/trimmed_data/DNA_trimmed/SRR4342133_2.paired.trimmed.fastq.gz \
--o ~/genome_analysis/GenomeAnalysis/analysis/02_assembly 2>D3_assembly_output
+-o D3_megahit_output 2>D3_assembly_output
