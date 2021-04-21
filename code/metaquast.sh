4c1663885424cf9032fff1b2d143de4c48f2dc31
Vivianne Inglis <vinglis@rackham3.uppmax.uu.se>
2021-04-21 23:21:38 +0200

    Updated metaquast script.

--- code/metaquast.sh
+++ code/metaquast.sh
@@ -7,9 +7,10 @@
 #SBATCH -J metaquast
 
 # Load modules
-module load bioinfo-tools quast
+module load bioinfo-tools 
+module load quast
 
 cd ~/genome_analysis/GenomeAnalysis/analysis/02_assembly/assembly_QC
 
-python metaquast.py ../D1_megahit_output/final.contigs.fa -o D1_metaquast_out
-python metaquast.py ../D3_megahit_output/final.contigs.fa -o D3_metaquast_out
+metaquast.py -o D1_metaquast_out ../D1_megahit_output/final.contigs.fa
+metaquast.py -o D3_metaquast_out ../D3_megahit_output/final.contigs.fa
