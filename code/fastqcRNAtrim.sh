e42c30ad2817701481b2975621affca0fb067505
Vivianne Inglis <vinglis@s70.uppmax.uu.se>
2021-04-21 16:28:08 +0200

    Ran fastqc on trimmed RNA reads

--- code/fastqcRNAtrim.sh
+++ code/fastqcRNAtrim.sh
@@ -1,6 +1,6 @@
 # Script to run fastQC on trimmed RNA reads
 
-for files in ~/genome_analysis/GenomeAnalysis/data/trimmed_data/RNA_trimmed/*
+for files in ~/genome_analysis/GenomeAnalysis/data/trimmed_data/RNA_trimmed/RNA*
 do
 fastqc $files -o ~/genome_analysis/GenomeAnalysis/analysis/01_preprocessing/fastqc_trim/RNA_trim
 done
