4e6d79eaf0ff4d6730c8d70b307fcc5e84f52069
Vivianne Inglis <vinglis@s19.uppmax.uu.se>
2021-05-24 11:23:01 +0200

    Updated prokka script

--- code/prokka.sh
+++ code/prokka.sh
@@ -1,15 +1,19 @@
-#!/bin/bash -l
-#SBATCH -A g2021012
-#SBATCH -M snowy
-#SBATCH -p core
-#SBATCH -n 2
-#SBATCH -t 03:00:00
-#SBATCH -J prokka
+# Bash script to run prokka on D1 & D3 final contigs
+# using options --metagenome (for fragmented contigs) and --cpus 2 (to specify 2 cores)
 
-# Load modules
 module load bioinfo-tools
 module load prokka
 
-cd ~/genome_analysis/GenomeAnalysis/analysis/04_annotation
+cd ~/genome_analysis/GenomeAnalysis/analysis/03_binning/D1_metabat_output/
+for D1_binfastafile in *.fa
+do
+prokka --outdir ~/genome_analysis/GenomeAnalysis/analysis/04_annotation/D1_prokka_output/"$D1_binfastafile"_prokka_out \
+--metagenome --cpus 2 $D1_binfastafile 2>~/genome_analysis/GenomeAnalysis/analysis/04_annotation/D1_prokka_output/"$D1_binfastafile"_annotation.out
+done
 
-
+cd ~/genome_analysis/GenomeAnalysis/analysis/03_binning/D3_metabat_output/
+for D3_binfastafile in *.fa
+do
+prokka --outdir ~/genome_analysis/GenomeAnalysis/analysis/04_annotation/D3_prokka_output/"$D3_binfastafile"_prokka_out \
+--metagenome --cpus 2 $D3_binfastafile 2>~/genome_analysis/GenomeAnalysis/analysis/04_annotation/D3_prokka_output/"$D3_binfastafile"_annotation.out
+done
